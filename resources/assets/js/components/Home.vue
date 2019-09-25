01f455f2d3c03ba60591590c39134487912d325d
Viacheslav <zhizhkoslava@gmail.com>
2019-09-25 10:14:22 +0300

    simple SPA with Laravel and Vue

---
+++ resources/assets/js/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Vue.JS SPA example</div>
+
+                    <div class="card-body">
+                        <h2>{{ welcome }}</h2>
+                        <img src="https://intsystem.org/wp-content/uploads/2013/05/php.png" height="500px" width="500px" alt="WeCode">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        computed: {
+            welcome() {
+                return this.$store.getters.welcome
+            }
+        }
+    }
+</script>
